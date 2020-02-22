867e278e0c0e99d1cd325168a3f6b9a423d95221
Gilberto Ocampo <ocampo@gate.net>
2020-02-22 20:34:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet (planeteer_calls)
 end
 
 def long_planeteer_calls (arr2)
-  arr2.any? |x| {x.length > 4}
+  arr2.any? {|x| x.length > 4}
 end
 
 def find_the_cheese# code an argument here
