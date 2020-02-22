a2fe7238bff855a00bc4a919e51dfd7b8e332c35
Gilberto Ocampo <ocampo@gate.net>
2020-02-22 20:19:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def summon_captain_planet (planeteer_calls)
   planeteer_calls
   planeteer_calls.map do |x|
-    x.capitalize 
+    x.capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
